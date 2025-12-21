39bcbe430fd3ecfe59ac2443f0e9c6bfea06bbd2
Polina Trevogina <polina.trevogina@gmail.com>
2025-12-21 15:57:32 +0300

    Add attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import utils.Attach;
 
 public class TestBase {
     @BeforeAll
@@ -15,5 +18,15 @@ public class TestBase {
         Configuration.headless = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo(); // Видео работает, если вы используете удаленный браузер (например, Selenoid)
+
+        Selenide.closeWebDriver();
     }
 }
