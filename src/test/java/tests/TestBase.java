d183f82de4e095e5e3d76b7bb53fce99a21cbd47
Polina Trevogina <polina.trevogina@gmail.com>
2025-12-23 15:08:46 +0300

    Add attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,17 +6,43 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import utils.Attach;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 5000;
-        Configuration.headless = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-browser-side-navigation");
+        chromeOptions.addArguments("--disable-gpu");
+
+        // Настройки для Selenoid (обязательно для видео)
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", true);
+        chromeOptions.setCapability("selenoid:options", selenoidOptions);
+
+        // Исправленное логирование
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.BROWSER, Level.ALL);
+        chromeOptions.setCapability("goog:loggingPrefs", logPrefs);
+
+        Configuration.browserCapabilities = chromeOptions; // Присваиваем напрямую опции
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
@@ -25,7 +51,7 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo(); // Видео работает, если вы используете удаленный браузер (например, Selenoid)
+        Attach.addVideo();
 
         Selenide.closeWebDriver();
     }
